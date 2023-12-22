de43114a21a0cbd44490c5a3fd73cd868524b90e
Ren Jia Li <rjl68666@163.com>
2023-12-22 16:43:10 +0800

    备份

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,20 @@
 <template>
-  <div></div>
+  <photo-sphere-viewer></photo-sphere-viewer>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PhotoSphereViewer from './components/photo-sphere-viewer.vue';
+</script>
 
-<style></style>
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+}
+</style>
---
+++ src/components/photo-sphere-viewer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div ref="viewer" class="photo-sphere-viewer"></div>
+</template>
+
+<script setup lang="ts">
+import { Viewer } from '@photo-sphere-viewer/core';
+import { GyroscopePlugin } from '@photo-sphere-viewer/gyroscope-plugin';
+import { onMounted, ref } from 'vue';
+const viewer = ref<HTMLElement>();
+
+onMounted(() => {
+  new Viewer({
+    plugins: [GyroscopePlugin],
+    container: viewer.value as HTMLElement,
+    panorama: '外围.jpg',
+    navbar: false
+  });
+});
+</script>
+<style lang="less" scoped>
+.photo-sphere-viewer {
+  width: 100vw;
+  height: 100vh;
+}
+</style>
