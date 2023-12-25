f45effacbf1038c339b3a769101ef528b654a08e
Ren Jia Li <rjl68666@163.com>
2023-12-25 12:28:56 +0800

    备份

--- src/components/index.vue
+++ src/components/index.vue
@@ -28,66 +28,63 @@ nextTick(() => {
     ]
   });
   const virtualTour: any = viewer.getPlugin(VirtualTourPlugin);
-  virtualTour.setNodes(
-    [
-      {
-        id: 'node-1',
-        panorama: '/外围.jpg',
-        position: [],
-        links: [
-          {
-            nodeId: 'node-2',
-            position: { textureX: -5000, textureY: 2500 },
-            linkOffset: { yaw: 90, pitch: 0, depth: 0 }
-          }
-        ]
-      },
-      {
-        id: 'node-2',
-        panorama: '/沙盘.jpg',
-        links: [
-          {
-            nodeId: 'node-1',
-            linkOffset: { yaw: -45, pitch: 0, depth: 1 },
-            position: { textureX: 2000, textureY: 2500 }
-          },
-          {
-            nodeId: 'node-3',
-            linkOffset: { yaw: -90, pitch: 0, depth: 2 },
-            position: { textureX: -5000, textureY: 2500 }
-          },
-          {
-            nodeId: 'node-4',
-            linkOffset: { yaw: 2, pitch: 0, depth: 3 },
-            position: { textureX: 2000, textureY: 2500 }
-          }
-        ]
-      },
-      {
-        id: 'node-3',
-        panorama: '/二楼.jpg',
-        links: [
-          {
-            nodeId: 'node-2',
-            linkOffset: { yaw: -90, pitch: 10, depth: 1 },
-            position: { textureX: -2500, textureY: 2500 }
-          }
-        ]
-      },
-      {
-        id: 'node-4',
-        panorama: '/public/大厅.jpg',
-        links: [
-          {
-            nodeId: 'node-2',
-            linkOffset: { yaw: -180, pitch: 0, depth: 0 },
-            position: { textureX: -2500, textureY: 2500 }
-          }
-        ]
-      }
-    ],
-    'node-2'
-  );
+  virtualTour.setNodes([
+    {
+      id: 'node-1',
+      panorama: '/外围.jpg',
+      position: [],
+      links: [
+        {
+          nodeId: 'node-2',
+          position: { textureX: -5000, textureY: 2500 },
+          linkOffset: { yaw: 90, pitch: 0, depth: 0 }
+        }
+      ]
+    },
+    {
+      id: 'node-2',
+      panorama: '/沙盘.jpg',
+      links: [
+        {
+          nodeId: 'node-1',
+          linkOffset: { yaw: -45, pitch: 0, depth: 1 },
+          position: { textureX: 2000, textureY: 2500 }
+        },
+        {
+          nodeId: 'node-3',
+          linkOffset: { yaw: -90, pitch: 0, depth: 2 },
+          position: { textureX: -5000, textureY: 2500 }
+        },
+        {
+          nodeId: 'node-4',
+          linkOffset: { yaw: 2, pitch: 0, depth: 3 },
+          position: { textureX: 2000, textureY: 2500 }
+        }
+      ]
+    },
+    {
+      id: 'node-3',
+      panorama: '/二楼.jpg',
+      links: [
+        {
+          nodeId: 'node-2',
+          linkOffset: { yaw: -90, pitch: 10, depth: 1 },
+          position: { textureX: -2500, textureY: 2500 }
+        }
+      ]
+    },
+    {
+      id: 'node-4',
+      panorama: '/public/大厅.jpg',
+      links: [
+        {
+          nodeId: 'node-2',
+          linkOffset: { yaw: -180, pitch: 0, depth: 0 },
+          position: { textureX: -2500, textureY: 2500 }
+        }
+      ]
+    }
+  ]);
 });
 </script>
 <style lang="less" scoped>
