23866b5caa60586b1ef2193a6fff05e9106e937f
Ren Jia Li <rjl68666@163.com>
2023-12-26 08:16:11 +0800

    备份

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<template></template>
+<template>
+  <photo-sphere-viewer></photo-sphere-viewer>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import PhotoSphereViewer from './components/index.vue';
+</script>
 
 <style></style>
---
+++ src/components/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="photo-sphere-viewer"></div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="less" scoped>
+.photo-sphere-viewer {
+  width: 100%;
+  height: 100%;
+}
+</style>
