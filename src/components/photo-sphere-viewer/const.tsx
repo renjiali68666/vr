7a4cac964c2e47b2ce67609e1a241dce77fea767
Ren Jia Li <rjl68666@163.com>
2023-12-27 10:27:50 +0800

    初始化全景图

--- src/components/photo-sphere-viewer/const.tsx
+++ src/components/photo-sphere-viewer/const.tsx
@@ -1,5 +1,6 @@
 import { ref } from 'vue';
 import type { Viewer } from '@photo-sphere-viewer/core';
+import type { MarkerConfig } from '@photo-sphere-viewer/markers-plugin';
 export const viewer = ref<Viewer>();
 export const nodes = [
   {
