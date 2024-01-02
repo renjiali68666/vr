200eca8deaabf329c06b52ffad68a9589fed2680
Ren Jia Li <rjl68666@163.com>
2024-01-02 10:26:55 +0800

    被分为你

--- src/components/photo-sphere-viewer/const.tsx
+++ src/components/photo-sphere-viewer/const.tsx
@@ -1,5 +1,5 @@
 import { ref } from 'vue';
-import type { Size, Viewer } from '@photo-sphere-viewer/core';
+import type { Viewer } from '@photo-sphere-viewer/core';
 import type { MarkerConfig } from '@photo-sphere-viewer/markers-plugin';
 import type { VirtualTourNode } from '@photo-sphere-viewer/virtual-tour-plugin';
 export const viewer = ref<Viewer>();
@@ -7,45 +7,14 @@ export const selectId = ref('');
 export const nodes = [
   {
     id: '1',
-    name: '客厅',
-    panorama: '/客厅.jpg',
-    markers: [
-      {
-        id: 'tv',
-        position: { yaw: '3deg', pitch: '3deg' },
-        html: '电视机',
-        scale: [0.5, 1.5],
-        className: 'text-bg-b'
-      },
-      {
-        id: 'shafa',
-        position: { yaw: '-52deg', pitch: '-8deg' },
-        html: '沙发',
-        scale: [0.5, 1.5],
-        className: 'text-bg-b'
-      }
-    ] as MarkerConfig[],
+    name: '渔田广场',
+    panorama: '/广场.jpg',
+    markers: [] as MarkerConfig[],
     links: [
       {
         nodeId: '2',
-        name: '厨房',
-        position: { yaw: '150deg', pitch: '0deg' },
-        markerStyle: {
-          size: { width: 36, height: 36 }
-        }
-      },
-      {
-        nodeId: '3',
-        name: '卫生间',
-        position: { yaw: '86deg', pitch: '0deg' },
-        markerStyle: {
-          size: { width: 36, height: 36 }
-        }
-      },
-      {
-        nodeId: '4',
-        name: '卧室',
-        position: { yaw: '-155deg', pitch: '0deg' },
+        name: '游客中心',
+        position: { yaw: '113deg', pitch: '2deg' },
         markerStyle: {
           size: { width: 36, height: 36 }
         }
@@ -54,25 +23,44 @@ export const nodes = [
   },
   {
     id: '2',
-    name: '厨房',
-    panorama: '/厨房.jpg',
+    name: '沙盘',
+    panorama: '/沙盘.jpg',
     links: [
       {
         nodeId: '1',
-        name: '客厅',
-        position: { yaw: '-90deg', pitch: '0deg' }
+        name: '渔田广场',
+        position: { yaw: '-180deg', pitch: '0deg' },
+        markerStyle: {
+          size: { width: 36, height: 36 }
+        }
+      },
+      {
+        nodeId: '3',
+        name: '大厅',
+        position: { yaw: '0deg', pitch: '-3deg' },
+        markerStyle: {
+          size: { width: 36, height: 36 }
+        }
+      },
+      {
+        nodeId: '4',
+        name: '二楼转角',
+        position: { yaw: '-78deg', pitch: '-1deg' },
+        markerStyle: {
+          size: { width: 36, height: 36 }
+        }
       }
     ]
   },
   {
     id: '3',
-    name: '卫生间',
-    panorama: '/洗手间.jpg',
+    name: '大厅',
+    panorama: '/大厅.jpg',
     links: [
       {
-        nodeId: '1',
-        name: '客厅',
-        position: { yaw: '120deg', pitch: '0deg' },
+        nodeId: '2',
+        name: '沙盘',
+        position: { yaw: '-120deg', pitch: '-1deg' },
         markerStyle: {
           size: { width: 36, height: 36 }
         }
@@ -81,13 +69,13 @@ export const nodes = [
   },
   {
     id: '4',
-    name: '卧室',
-    panorama: '/卧室.jpg',
+    name: '二楼转角',
+    panorama: '/二楼转角.jpg',
     links: [
       {
-        nodeId: '1',
-        name: '客厅',
-        position: { yaw: '105deg', pitch: '0deg' },
+        nodeId: '2',
+        name: '沙盘',
+        position: { yaw: '-10deg', pitch: '-20deg' },
         markerStyle: {
           size: { width: 36, height: 36 }
         }
